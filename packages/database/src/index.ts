a4705dec6752d54caf4c5e09b517432a5e85cf3e
satwik <mohanty.satwik290@gmail.com>
2026-01-13 20:13:26 +0530

    connect pg admin with prisma

---
+++ packages/database/src/index.ts
@@ -0,0 +1,21 @@
+// packages/database/src/index.ts
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+export * from '@prisma/client';
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, // Pass the adapter here
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
